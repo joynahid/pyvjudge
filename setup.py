100074af4a1ce5d081a90746748ce0b354dd93cf
Nahid Hasan <nahidhasan282@gmail.com>
2022-01-21 03:17:21 +0600

    build: add setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+      name='pyvjudge',
+      version='0.0.1',
+      python_requires=">=3.7",
+      description='Python Vjudge Client',
+      author='Nahid Hasan',
+      author_email='nahidhasan282@gmail.com',
+      url='',
+      packages=find_packages(),
+      )
