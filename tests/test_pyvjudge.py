499f84f7b15fa445796825db2d1e1f2facc9f01f
Nahid Hasan <nahidhasan282@gmail.com>
2022-01-21 03:14:37 +0600

    feat: add PyVjudge class to interact with different internal apis to generate contest info and stuffs

---
+++ tests/test_pyvjudge.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestPyVjudge(unittest.TestCase):
+    def __init__(self, *args) -> None:
+        super().__init__(*args)
+
+    def test_get_standing(self):
+        pass
+
+    def test_find_contest_id(self):
+        pass
