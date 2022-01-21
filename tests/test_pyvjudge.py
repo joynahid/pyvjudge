23d958dfbaad53933f3a67808867c953e152fa61
Nahid Hasan <nahidhasan282@gmail.com>
2022-01-21 20:24:36 +0600

    test: add pyvjudge tests

--- tests/test_pyvjudge.py
+++ tests/test_pyvjudge.py
@@ -1,12 +1,17 @@
 import unittest
 
+from pyvjudge import PyVjudge
+from pyvjudge.models.contest import Standing
+
 
 class TestPyVjudge(unittest.TestCase):
     def __init__(self, *args) -> None:
+        self.pv = PyVjudge()
         super().__init__(*args)
 
     def test_get_standing(self):
-        pass
+        standing = self.pv.get_standing(PyVjudge.find_contest_id("https://vjudge.net/contest/458956#overview"))
+        assert isinstance(standing, Standing)
 
     def test_find_contest_id(self):
-        pass
+        assert PyVjudge.find_contest_id("https://vjudge.net/contest/458956#overview") == 458956
