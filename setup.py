d6a52ebeca1644280a5e1bf3f5884f77cbb72b39
Nahid Hasan <nahidhasan282@gmail.com>
2022-08-03 13:45:21 +0600

    fix: setup requirements

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from setuptools import setup, find_packages
 from pyvjudge import __version__
 
 setup(
-      name='pyvjudge',
-      version=__version__,
-      python_requires=">=3.7",
-      description='Python Vjudge Client',
-      author='Nahid Hasan',
-      author_email='nahidhasan282@gmail.com',
-      url='',
-      packages=find_packages(),
-      )
+    name="pyvjudge",
+    version=__version__,
+    python_requires=">=3.7",
+    description="Python Vjudge Client",
+    author="Nahid H.",
+    author_email="info@nahidhq.com",
+    url="",
+    packages=find_packages(),
+    install_requires=["httpx>=0.23.0", "beautifulsoup4>=4.11.1"],
+)
